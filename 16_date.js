bbb25fcad38ba83b1541ebc33b8c9d49bb82e082
Nahidul-Islam <mdnahid784055@gmail.com>
2025-05-07 06:56:14 +0600

    adding date topic

---
+++ 16_date.js
@@ -0,0 +1,8 @@
+let time = new Date(2005,9,18);
+console.log(time.toDateString());
+console.log(time.toUTCString());
+console.log(time.toISOString());
+console.log(Date.parse(2025, 4, 6));
+console.log(time.getFullYear());
+console.log(time.setFullYear(2024));
+
